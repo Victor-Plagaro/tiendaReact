4299623ebaec8a2b2d749d146c13eeb03a965390
Victor <victorplagaromoreno@alumno.ieselrincon.es>
2025-01-17 13:43:18 +0000

    Add: react router DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom'
 import Header from './Header'
 import Products from './Products'
 
@@ -8,10 +6,14 @@ function App() {
 
   return (
     <>
-    <section>
-      <Header />
-      <Products />
-    </section>
+      <div>
+        <Header />
+        <div>
+          <Routes>
+            <Route path='/tienda' element={<Products></Products>}></Route>
+          </Routes>
+        </div>
+      </div>
     </>
   )
 }
